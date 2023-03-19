233f5818ff9ec51024c5e6962c1d2fb76f30db36
EstellaSaveljeva221RDB176 <124592117+EstellaSaveljeva221RDB176@users.noreply.github.com>
2023-03-20 00:30:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,17 +19,19 @@ def parallel_processing(n, m, data):
     return output
 
 def main():
-    n = 0
-    m = 0
-
-    data = []
-
-    
+    n, m = map(int, input().split())
+    if not(1 <= n <= 10**5):
+        raise ValueError("n range is between 1 and 10^5")
+    if not(1 <= m <= 10**5):
+        raise ValueError("m range is between 1 and 10^5")
+    data = list(map(int, input().split()))
+    for i in range(m):
+        if not(0 <= data[i] <= 10**9):
+            raise ValueError("Elements between 0 and 10^9")
     result = parallel_processing(n,m,data)
     
-    
-
-
+    for i in range(m):
+        print(result[i][0], result[i][1]
 
 if __name__ == "__main__":
     main()
