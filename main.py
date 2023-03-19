f15031760f46fecb194c9a85f2360060131a5a6f
EstellaSaveljeva221RDB176 <124592117+EstellaSaveljeva221RDB176@users.noreply.github.com>
2023-03-20 00:34:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ def parallel_processing(n, m, data):
     output = []
     nexthread = 0
     numthread = [0] * n
-     for i in range(m):
-            nexthread=0
-            for j in range (1,n):
-                if numthread[j]<numthread[nexthread]:
-                    nextread = j
+    for i in range(m):
+        nexthread=0
+        for j in range (1,n):
+            if numthread[j]<numthread[nexthread]:
+                nextread = j
                     
         start = numthread[nexthread]
         end = start + data[i]
